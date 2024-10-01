c8f047507672b715fe716cd65359b413790d8f01
Giaganatasaputra <amenksiuu@gmail.com>
2024-10-01 13:12:30 +0800

    Create list.py

---
+++ list.py
@@ -0,0 +1,30 @@
+# inisiasi list
+my_list = ["prodi ti",1,2,3,4,2,1]
+
+
+# mengetahui panjang list
+panjang_my_list = len(my_list)
+
+
+# cetak list
+print ("my_list  ",my_list)
+print ("panjang list : ",panjang_my_list)
+print("panggil index ke 5: ",my_list[5])
+
+# tambah anggota di dalam list
+my_list.append("keren")
+
+# cetak list setelah input anggota baru
+print ("my_list setelah input nilai baru: ",my_list)
+
+# menghapus nilai 2 di dalam list
+my_list.remove(2)
+
+# cetak list setelah menghapus nilai 2 yang pertama 
+print("my_list setelah menghapus nilai 2: ",my_list)
+
+# menghapus index ke 1
+del my_list[1]
+
+# cetak list setelah index ke 1
+print("my_list setelah menghapus index ke 1: ",my_list)
